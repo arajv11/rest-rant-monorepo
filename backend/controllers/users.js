f64abbe915622bc58c5f323ae9a916571d09ac15
Anish Raj <a.rajv11@gmail.com>
2022-10-09 19:29:15 -0400

    commit

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrpyt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    let { password, ...rest } = req.body;
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcrpyt.hash(password, 10)
+    })
     res.json(user)
 })
 
